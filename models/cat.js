3fcdccc4d605b90b19e320e2ba836b19d4efed74
Lucas Epps <lucasbepps@gmail.com>
2018-05-10 18:44:44 -0600

    adding routes

--- models/cat.js
+++ models/cat.js
@@ -1,29 +1,23 @@
-module.exports = function (sequelize, DataTypes) {
-
-
-    var Cat = sequelize.define("Cat",
-        {
-            name: {
-                type: DataTypes.STRING
-            },
-
-            hp: {
-                type: DataTypes.INTEGER
-            },
-
-            atk: {
-                type: DataTypes.INTEGER
-            },
-
-            def: {
-                type: DataTypes.INTEGER
-            }
-
-
-        }, {
-            timestamps: false
-        }
-    );
-
-    return Cat;
-}
+module.exports = function(sequelize, DataTypes) {
+  var Cat = sequelize.define(
+    "Cat",
+    {
+      name: {
+        type: DataTypes.STRING
+      },
+      hp: {
+        type: DataTypes.INTEGER
+      },
+      atk: {
+        type: DataTypes.INTEGER
+      },
+      def: {
+        type: DataTypes.INTEGER
+      }
+    },
+    {
+      timestamps: false
+    }
+  );
+  return Cat;
+};
